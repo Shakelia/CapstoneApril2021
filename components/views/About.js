9263f1b66e5f528396e532b2d54ea4273b9ba820
Shakelia Bolden <shakelia_bolden@hotmail.com>
2021-06-20 15:17:36 -0500

    6/20

---
+++ components/views/About.js
@@ -0,0 +1,28 @@
+export default () => `
+<h2>Welcome To Kidney Konnect</h2>
+<div class="flex aboutBox">
+<!--First text box-->
+    <h3 class="boxTitles">
+        Kidney and Dialysis Patients
+    </h3>
+    <p>
+        The pandemic was hard on everyone. What if you already had a weakened immune system? I know exactly what you may have felt. Confused, afraid and isolated. I myself had been doing home dialysis for a little more than a year when coronavirus came to be. I could no longer do things such as go to the grocery store or meet up with friend for lunch. The smallest task such as running to the local dollar store was no longer an option. I started to isolate at home. I couldn't go out and no one could come in.
+    </p>
+</div>
+<div class="flex aboutBox">
+    <!--Second text box-->
+        <h3 class="boxTitles">
+            So what could you do?
+        </h3>
+        <p>
+            Would you like to have a space that connects you with someone that is open to a phone call, text or even having lunch with you? I know I definitly could've used that before and after I received a kidney transplant. Especially with people that specifically knew about my disease. My idea of Kidney Konnect was always a thought. I just never knew how I would bring it to life. Enter Savvy Coders. I've always been interested in how websites came to life. Savvy Coders would provide me with the knowledge to bring my idea to life.
+        </p>
+</div>
+<div class="flex aboutBox">
+    <!--Third text box-->
+        <h3 class="boxTitles">
+            Medical Professionals
+        </h3>
+        <p>
+           Well I would start at the very place journey began, with my nephrologist. My nephrologist was with me every step of the way. He listened and anticipated where this journey was taking me. He is my doctor and my friend. Next would be my dialysis nurse. I was so nervous about if I would actually be able to do home dialysis. My nurse assured me that if I followed what she taught me that I would be successful. Last but not least, twin sister. She has been a facility director for a dialysis company for years. She always told me that when the time came for me to start dialysis that I should choose home peritoneal dialysis and so I did.
+        </p>
--- store/Home.js
+++ store/Home.js
@@ -1,4 +1,4 @@
 export default {
-  header: "Home",
+  header: "Kidney Konnect",
   view: "Home"
 }
