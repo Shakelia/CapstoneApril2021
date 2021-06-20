9b72979ad861c41fa39ada7589e816f8464bda3f
Shakelia Bolden <shakelia_bolden@hotmail.com>
2021-06-20 18:26:03 -0500

    Contact

--- components/views/About.js
+++ components/views/About.js
@@ -15,7 +15,7 @@ export default () => `
             So what could you do?
         </h3>
         <p>
-            Would you like to have a space that connects you with someone that is open to a phone call, text or even having lunch with you? I know I definitly could've used that before and after I received a kidney transplant. Especially with people that specifically knew about my disease. My idea of Kidney Konnect was always a thought. I just never knew how I would bring it to life. Enter Savvy Coders. I've always been interested in how websites came to life. Savvy Coders would provide me with the knowledge to bring my idea to life.
+            Would you like to have a space that connects you with someone that is open to a phone call, text or even having lunch with you? I know I definitly could've used that before and after I received a kidney transplant. Especially with people that specifically knew about my disease. My idea of Kidney Konnect was always a thought since being diagnosed with chronic kidney disease at the age of 20. I just never knew how I would bring it to life. Enter Savvy Coders. I've always been interested in knowing how websites came to life. Savvy Coders would provide me with the knowledge to bring my idea to life.
         </p>
 </div>
 <div class="flex aboutBox">
@@ -26,3 +26,14 @@ export default () => `
         <p>
            Well I would start at the very place journey began, with my nephrologist. My nephrologist was with me every step of the way. He listened and anticipated where this journey was taking me. He is my doctor and my friend. Next would be my dialysis nurse. I was so nervous about if I would actually be able to do home dialysis. My nurse assured me that if I followed what she taught me that I would be successful. Last but not least, twin sister. She has been a facility director for a dialysis company for years. She always told me that when the time came for me to start dialysis that I should choose home peritoneal dialysis and so I did.
         </p>
+
+
+<div class="flex aboutBox">
+    <!--Second text box-->
+        <h3 class="boxTitles">
+            Feel like a burden no longer
+        </h3>
+        <p>
+            Do you feel like you might be overwhelming to your caretakers and family? I know for myself having to depend on someone else for literally for everything was a difficult transition. I didn't want to add one more thing to their plate. I was missing socializing and talking about something other than my health issues. I intend for Kidney Konnect to assist in finding that connection. Maybe you're only comfortable with a phone call, that's just fine. A volunteer may not have time to come over but can check in a few times a month.
+        </p>
+</div>
