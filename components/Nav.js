9948dddeddcf78cad6b4598e6023af5f5ae31e5e
Shakelia Bolden <shakelia_bolden@hotmail.com>
2021-06-21 20:25:10 -0500

    Edits

--- components/Nav.js
+++ components/Nav.js
@@ -1,14 +1,15 @@
-
-import html from "html-literal";
-
-export default (links) => html`
-  <nav>
-    <i class="fas fa-bars"></i>
-    <ul class="hidden--mobile nav-links">
-      ${links.map(
-        (el) =>
-          `<li><a href="/${el.title}" title="${el.title}" data-navigo>${el.text}</a></li>`
-      )}
-    </ul>
-  </nav>
+export default links => `
+<nav>
+  <i class="fas fa-bars"></i>
+  <ul class="hidden--mobile nav-links">
+  ${links
+    .map(
+      link =>
+        `<li><a href="/${link.title !== "Home" ? link.title : ""}" title="${
+          link.title
+        }" data-navigo>${link.text}</a></li>`
+    )
+    .join("")}
+  </ul>
+</nav>
 `;
