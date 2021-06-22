cb0b565a1071b41bbeb15ab0e5a52cabebd4b02d
Shakelia Bolden <shakelia_bolden@hotmail.com>
2021-06-21 20:02:19 -0500

    Edits

---
+++ store/Header.js
@@ -0,0 +1,11 @@
+import { User } from "../store";
+
+export default () => `
+<!------------- header--------------->
+<div class="main-header">
+    <div class="header">
+        <h6>
+           Kidney Konnect is where you find a friendly Konnection!
+        </h6>
+    </div>
+    `;
