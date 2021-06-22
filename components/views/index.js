c4b3d94862e37fd6f88fd61732aa91f36d920572
Shakelia Bolden <shakelia_bolden@hotmail.com>
2021-06-21 20:54:11 -0500

    Edits

--- components/views/index.js
+++ components/views/index.js
@@ -1,4 +1,22 @@
-export { default as Header } from "./Header";
-export { default as Nav } from "./Nav";
-export { default as Main } from "./Main";
-export { default as Footer } from "./Footer";
+import { Header, Nav, Main, Footer } from "./components";
+import * as state from "./store";
+// import {Placeholder for files from ./lib};
+
+function render(st) {
+  document.querySelector("#root").innerHTML = `
+    ${Header(st)}
+    ${Nav(state.Links)}
+    ${Main(st)}
+    ${Footer(st)}`;
+  addEventListeners();
+}
+
+render(state.Home);
+
+// menu toggle to hamburger
+
+function addEventListeners() {
+  document.querySelector(".fa-bars").addEventListener("click", () => {
+    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+  });
+}
